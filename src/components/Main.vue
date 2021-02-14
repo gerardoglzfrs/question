bfa5c6726173f3e363a2978e21154fbc3e03efc3
gerardoglzfrs <gerardo210819@gmail.com>
2021-02-14 10:07:18 -0600

    diseño listo

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -78,7 +78,7 @@
                         <v-card flat>
                             <v-card-text>
                                 <v-row class="justify-center mt-6 mb-6">
-                                    <!-- Faltaaaa -->
+                                    <!-- Falta-->
                                 </v-row>
                             </v-card-text>
                         </v-card>
